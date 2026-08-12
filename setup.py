58b005f7cb04e7033ab233d815420cb570b93239
Lars Bilke <lars.bilke@ufz.de>
2026-08-12 13:25:43 +0200

    Update cmakelang dependency to a specific URL

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup
 setup(
     name="cmakelang-precommit-dummy",
     version="0.0.0",
-    install_requires=["cmakelang==0.6.13"]
+    install_requires=[
+        # Includes fixes for FILE_SETS
+        "cmakelang @ https://github.com/solarispika/cmake_format/releases/download/v0.6.13.2/cmakelang-0.6.13.2-py3-none-any.whl"
+    ],
 )
